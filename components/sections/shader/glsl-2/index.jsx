e20f3065976392d3c9a57e80773e34dad958afad
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-01-21 23:09:42 +0900

    Feat: Add glsl test2

--- components/sections/shader/glsl-2/index.jsx
+++ components/sections/shader/glsl-2/index.jsx
@@ -1,21 +1,30 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
+import { Leva } from "leva";
 
 const SectionGlsl2 = () => {
   //-268 130 -1051
+  //-1000 80 -993
+  // neder -17.5 / 65 / -140
+  // -2.5 / 100 / 0
+  // https://www.youtube.com/watch?v=f4s1h2YETNY&t=818s
   return (
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 55,
-        position: [0, 0, 12],
-      }}
-      shadows
-    >
-      <Experience />
-    </Canvas>
+    <>
+      <Leva hidden={location.hash !== '#debug'} />
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 55,
+          position: [0, 0, 12],
+        }}
+        shadows
+      >
+        <Experience />
+      </Canvas>
+    </>
+
   );
 };
 
--- components/sections/shader/glsl-2/shader/fragmentShader.glsl
+++ components/sections/shader/glsl-2/shader/fragmentShader.glsl
@@ -2,26 +2,43 @@ varying vec2 vUv;
 uniform vec2 uResolution;
 uniform float uTime;
 
+vec3 palette(float t) {
+    vec3 a = vec3(0.5);
+    vec3 b = vec3(0.5);
+    vec3 c = vec3(1.0);
+    vec3 d = vec3(0.263, 0.416, 0.557);
+    return a + b * cos(6.28318 * (c * t + d));
+}
+
 void main() {
     vec2 st = vUv;
-    
     st -= 0.5;
     st *= 2.0;
-
     st.x *= uResolution.x / uResolution.y;
-    float d = length(st);
-    // d = sin(d);
-    // d = abs(d);
 
-    // d = smoothstep(0.0, 0.1, d);
+    vec2 st0 = st;
+    vec3 finalColor = vec3(0.0);
 
-    d = sin(d*8.0+uTime)/8.0;
-    // d -= 0.5;
-    d = abs(d);
-    // d = step(0.1, d);
+    for(float i = 0.0 ; i < 4.0 ; i++) {
+        st = fract(st * 1.5) - 0.5;
 
-    d = smoothstep(0.0,0.1, d);
-    vec3 color = vec3(d,d,d);
+        float d = length(st) * exp(-length(st0));
+        vec3 col = palette(length(st0) + i*0.4 + uTime * 0.4);
 
-    gl_FragColor = vec4(color, 1.0);
+        // d = sin(d);
+        // d = abs(d);
+
+        // d = smoothstep(0.0, 0.1, d);
+
+        d = sin(d * 8.0 + uTime) / 8.0;
+        // d -= 0.5;
+        d = abs(d);
+        // d = step(0.1, d);
+
+        // d = smoothstep(0.0,0.1, d);
+        d = pow(0.01 / d, 1.2);
+        finalColor += col * d;
+    }
+
+    gl_FragColor = vec4(finalColor, 1.0);
 }
