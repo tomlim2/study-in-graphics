79eab1dcf277a541d7877072c12ac0313c74b643
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-02-12 11:58:30 +0900

    Feat: Add uniform value and update time

--- components/sections/shader/glsl-6/Experience.jsx
+++ components/sections/shader/glsl-6/Experience.jsx
@@ -3,14 +3,54 @@ import {
 } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { useControls } from "leva";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const Experience = () => {
+  const materialRef = useRef();
   const { color, opacity } = useControls("contact shadows", {
     color: "#1d8f75",
     opacity: { value: 1, min: 0, max: 1 },
   });
 
-  const glslTestMaterial = <meshStandardMaterial opacity={opacity} color={color} transparent onBeforeCompile={(shader) => { console.log(shader) }} />
+  useFrame((state, delta) => {
+    if (materialRef.current) {
+      customUniforms.uTime.value = state.clock.elapsedTime;
+      // console.log(materialRef.current.onBeforeCompile((shader)=>{
+      //   console.log(shader);
+      // }));
+      // materialRef.current.uniforms.uTime.value = state.clock.elapsedTime;
+      // console.log(materialRef.current.uniforms.uTime.value, "hi");
+    }
+  });
+
+  const customUniforms = {
+    uTime: {
+      value: 0
+    }
+  }
+
+  const onUpdateShader = (shader) => {
+    // glslTestMaterial.userData.shader = shader;
+    shader.uniforms.uTime = customUniforms.uTime;
+    // console.log(shader.fragmentShader)
+    shader.fragmentShader = shader.fragmentShader.replace(
+      /*glsl*/`#include <color_pars_fragment>`,
+      /*glsl*/`#include <color_pars_fragment>
+        uniform float uTime;
+      `)
+    shader.fragmentShader = shader.fragmentShader.replace(
+      /*glsl*/`#include <color_fragment>`,
+      /*glsl*/`#include <color_fragment>
+        diffuseColor = vec4(1,sin(uTime),0,1);
+      `)
+    // console.log(shader.fragmentShader)
+  }
+
+
+
+  const glslTestMaterial = <meshStandardMaterial ref={materialRef} opacity={opacity} color={color} transparent
+    onBeforeCompile={(shader) => onUpdateShader(shader)} />
 
   return (
     <>
