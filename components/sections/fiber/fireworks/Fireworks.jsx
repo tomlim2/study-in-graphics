00de80b7779d79f90a0a425eacb459b1ce434292
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-03-05 22:54:26 +0900

    Feat: Abjust shader material

--- components/sections/fiber/fireworks/Fireworks.jsx
+++ components/sections/fiber/fireworks/Fireworks.jsx
@@ -1,11 +1,19 @@
 import { useLoader } from "@react-three/fiber";
+import fireworkFragmentShader from 'raw-loader!glslify-loader!shaders/fireworks/fragment.glsl'
+import fireworkVertexShader from 'raw-loader!glslify-loader!shaders/fireworks/vertex.glsl'
+import { useEffect } from "react";
 import * as THREE from "three"
 
 
 export default function Fireworks() {
     const heyMap = useLoader(THREE.TextureLoader, '/assets/images/thumbnails/test-glsl-1.png')
-    const count = 20
+    const count = 600
     const positionsArray = new Float32Array(count * 3);
+    const sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+    sizes.resolution = new THREE.Vector2(sizes.width, sizes.height)
 
     for (let i = 0; i < count; i++) {
         const i3 = i * 3
@@ -17,6 +25,23 @@ export default function Fireworks() {
 
     console.log(positionsArray);
 
+    useEffect(() => {
+        const handleResize = () => {
+            // Update sizes
+            sizes.width = window.innerWidth
+            sizes.height = window.innerHeight
+            sizes.resolution.set(sizes.width, sizes.height)
+        };
+
+        // Add event listener when component mounts
+        window.addEventListener('resize', handleResize);
+
+        // Clean up event listener when component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return <>
         <points>
             <bufferGeometry >
@@ -27,13 +52,16 @@ export default function Fireworks() {
                     itemSize={3} //dikeranakan telah diketahui bahwa tiap arraytype axis akan berisi 3 value pada 1d array
                 />
             </bufferGeometry>
-            <pointsMaterial
-                map={heyMap}
-                sizes={0.5}
-                sizeAttenuation //merupakan parameter yang menscale object berdasarkan perspective camera
-                transparent={false}
-                alphaTest={0.5} //merupakan thresshold saat rendering untuk mencega bila opacity dibawah value alphatest
-                opacity={1.0}
+            <shaderMaterial
+                vertexShader={fireworkVertexShader}
+                fragmentShader={fireworkFragmentShader}
+                uniforms={
+                    {
+                        uSize: new THREE.Uniform(50),
+                        uResolution: new THREE.Uniform(new THREE.Vector2(sizes.resolution))
+                    }
+                }
+
             />
         </points>
     </>
---
+++ shaders/fireworks/vertex.glsl
@@ -0,0 +1,12 @@
+uniform float uSize;
+
+void main()
+{
+    // Final position
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    gl_Position = projectionMatrix * viewPosition;
+    // Final size
+    gl_PointSize = uSize;
+    gl_PointSize *= 1.0 / - viewPosition.z;
+}
