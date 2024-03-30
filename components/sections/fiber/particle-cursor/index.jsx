39d82c06fc47e5df6fae29ac10c0850968a7f13d
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-03-31 00:44:25 +0900

    Feat: Fill the canvas and context

--- components/sections/fiber/particle-cursor/index.jsx
+++ components/sections/fiber/particle-cursor/index.jsx
@@ -2,29 +2,54 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
 import { Leva, useControls } from "leva";
-import * as THREE from "three";
 import { Perf } from "r3f-perf";
 import { useRecoilValue } from "recoil";
 import { isDebuggerState } from "@/stores/storeFiber"
+import { useEffect, useRef } from "react";
 import styles from "./ParticleCursor.module.scss";
+import { ACESFilmicToneMapping, CineonToneMapping, CustomToneMapping, ReinhardToneMapping } from "three";
 
 const SectionParticleCursor = () => {
+  const canvasRef = useRef();
   const canvasConfig = useControls("canvas", {
     toneMapping: {
-      value: THREE.CustomToneMapping,
+      value: CustomToneMapping,
       options: {
-        ACESFilmic: THREE.ACESFilmicToneMapping,
-        Cineon: THREE.CineonToneMapping,
-        Reinhard: THREE.ReinhardToneMapping,
-        Custom: THREE.CustomToneMapping,
+        ACESFilmic: ACESFilmicToneMapping,
+        Cineon: CineonToneMapping,
+        Reinhard: ReinhardToneMapping,
+        Custom: CustomToneMapping,
       },
     },
     toneMappingExposure: { value: 1.5, min: 0, max: 3, step: 0.01 },
   });
   const isDebugger = useRecoilValue(isDebuggerState)
+  useEffect(() => {
+    /**
+   * Displacement
+   */
+    const displacement = {}
+
+    // 2D canvas
+    displacement.canvas = document.createElement('canvas')
+    displacement.canvas.width = 128
+    displacement.canvas.height = 128
+    displacement.canvas.style.position = 'fixed'
+    displacement.canvas.style.width = '512px'
+    displacement.canvas.style.height = '512px'
+    displacement.canvas.style.top = '50px'
+    displacement.canvas.style.left = '20px'
+    displacement.canvas.style.zIndex = 10
+    document.body.append(displacement.canvas)
+
+    // Context
+    displacement.context = displacement.canvas.getContext('2d')
+    displacement.context.fillStyle = 'red'
+    displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
+
+  }, [])
   return (
     <>
-      <canvas className={styles.displacement} width={128} height={128}></canvas>
       {!isDebugger && <Leva hidden={true} />}
       <Canvas
         camera={{
