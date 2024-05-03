b203cd25ef1c67d6545fd40f4da17516af56580f
deemo <deemo@cinamon.io>
2024-05-03 14:48:45 +0900

    feat: add glsl 9 on shaders

--- components/sections/home/homeContentLists.js
+++ components/sections/home/homeContentLists.js
@@ -3,6 +3,11 @@ export const inProgressList = [
     categoryName: "In progress",
     urlList: [
       {
+        name: "glsl 9",
+        url: "/shader/glsl-9",
+        thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
+      },
+      {
         name: "Dynamic wall in Blender",
         url: "/fiber/journey-game3",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
