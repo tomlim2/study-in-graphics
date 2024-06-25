dcce0c6024de8db9e79be4574945470e41b6a1c9
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-06-25 23:21:11 +0900

    feat: add model

--- components/sections/fiber/sliced-model/MeshSample.jsx
+++ components/sections/fiber/sliced-model/MeshSample.jsx
@@ -1,14 +1,17 @@
+import { useGLTF } from "@react-three/drei"
 import { Vector3 } from "three"
 
 const MeshSample = () => {
-
+    const grears = useGLTF('/sliced-model/gears.glb')
+    let geometry = grears.scene.children[0].geometry
     return (
         <>
-            <mesh position={[0, 0, 0]} receiveShadow>
-                <icosahedronGeometry args={[1.5, 64]} />
+            {/* <mesh position={[0, 0, 0]} receiveShadow>
+                <bufferGeometry {...geometry}></bufferGeometry>
                 <meshStandardMaterial metalness={.5} roughness={.25} envMapIntensity={.5} color={'#858080'}></meshStandardMaterial>
-            </mesh>
-            <mesh lookAt={new Vector3(0, 0, 0)} position={[-4, -3, -4]} >
+            </mesh> */}
+            <primitive object={grears.scene} />
+            <mesh  position={[-4, -3, -4]} lookAt={new Vector3(0,0,0)} >
                 <planeGeometry args={[10, 10, 10]}></planeGeometry>
                 <meshStandardMaterial color={'#aaaaaa'}></meshStandardMaterial>
             </mesh>
