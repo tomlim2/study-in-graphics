c06c3b9ade6eb2ede78205b79bd0607055ba2ea2
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-07-03 23:48:35 +0900

    feat: update gears

--- components/sections/fiber/sliced-model/MeshSample.jsx
+++ components/sections/fiber/sliced-model/MeshSample.jsx
@@ -3,14 +3,24 @@ import { Vector3 } from "three"
 
 const MeshSample = () => {
     const grears = useGLTF('/sliced-model/gears.glb')
-    let geometry = grears.scene.children[0].geometry
+    let geometry1 = grears.scene.children[0].geometry
+    let geometry2 = grears.scene.children[1].geometry
+    let geometry3 = grears.scene.children[2].geometry
+    console.log(grears.scene);
     return (
         <>
-            {/* <mesh position={[0, 0, 0]} receiveShadow>
-                <bufferGeometry {...geometry}></bufferGeometry>
+            <mesh position={[0, 0, 0]} scale={[4,4,4]} receiveShadow castShadow>
+                <bufferGeometry {...geometry1}></bufferGeometry>
                 <meshStandardMaterial metalness={.5} roughness={.25} envMapIntensity={.5} color={'#858080'}></meshStandardMaterial>
-            </mesh> */}
-            <primitive object={grears.scene} />
+            </mesh>
+            <mesh position={[0, 0, 0]} receiveShadow castShadow>
+                <bufferGeometry {...geometry2}></bufferGeometry>
+                <meshStandardMaterial metalness={.5} roughness={.25} envMapIntensity={.5} color={'#858080'}></meshStandardMaterial>
+            </mesh>
+            <mesh position={[0, 0, 0]} receiveShadow castShadow>
+                <bufferGeometry {...geometry3}></bufferGeometry>
+                <meshStandardMaterial metalness={.5} roughness={.25} envMapIntensity={.5} color={'#858080'}></meshStandardMaterial>
+            </mesh>
             <mesh  position={[-4, -3, -4]} lookAt={new Vector3(0,0,0)} >
                 <planeGeometry args={[10, 10, 10]}></planeGeometry>
                 <meshStandardMaterial color={'#aaaaaa'}></meshStandardMaterial>
