a276ae0f35072ef0b13f61dd72ca71f93425991c
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-07-05 23:41:59 +0900

    feat: add vertex and fragment shader

--- components/sections/fiber/sliced-model/MeshSample.jsx
+++ components/sections/fiber/sliced-model/MeshSample.jsx
@@ -1,16 +1,21 @@
 import CustomShaderMaterial from 'three-custom-shader-material/vanilla'
 import { useGLTF } from "@react-three/drei"
 import { MeshPhysicalMaterial, Vector3 } from "three"
+import fragmentShader from 'raw-loader!glslify-loader!shaders/sliced/fragment.glsl'
+import vertexShader from 'raw-loader!glslify-loader!shaders/sliced/vertex.glsl'
 
 const MeshSample = () => {
     const grears = useGLTF('/sliced-model/gears.glb')
     let geometry1 = grears.scene.children[0].geometry
     let geometry2 = grears.scene.children[1].geometry
     let geometry3 = grears.scene.children[2].geometry
-    
-    const slicedMaterial =  new CustomShaderMaterial({
+
+    const slicedMaterial = new CustomShaderMaterial({
         // CSM
         baseMaterial: MeshPhysicalMaterial,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+
         metalness: 0.5,
         roughness: 0.25,
         envMapIntensity: 0.5,
