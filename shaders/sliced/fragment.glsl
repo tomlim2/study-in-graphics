dd220380f5c5d78be777f4dec49853967c114c53
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-07-09 22:26:39 +0900

    feat: add slice model

--- shaders/sliced/fragment.glsl
+++ shaders/sliced/fragment.glsl
@@ -4,6 +4,11 @@ void main()
 {
     // csm_FragColor = vec4(vPosition, 1.0);
     float angle = atan(vPosition.y, vPosition.x);
+    float uSliceStart = 1.0;
+    float uSliceArc = 1.5;
+
+    if(angle > uSliceStart && angle < uSliceStart + uSliceArc)
+        discard;
 
     csm_FragColor = vec4(vec3(angle), 1.0);
 }
--- shaders/sliced/vertex.glsl
+++ shaders/sliced/vertex.glsl
@@ -2,8 +2,12 @@ varying vec3 vPosition;
 
 void main()
 {
-    float uSliceStart = 1.0;
-    float uSliceArc = 1.5;
-    
     vPosition = csm_Position.xyz;
+    // float uSliceStart = 1.0;
+    // float uSliceArc = 1.5;
+
+    // float angle = atan(vPosition.y, vPosition.x);
+    // if(angle > uSliceStart && angle < uSliceStart + uSliceArc)
+    //     discard;
+    
 }
