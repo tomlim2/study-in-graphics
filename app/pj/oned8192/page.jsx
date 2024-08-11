321483289350468e4e2fb035769834bbf963b885
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-08-11 22:27:12 +0900

    feat: add base

---
+++ app/pj/oned8192/page.jsx
@@ -0,0 +1,18 @@
+import SectionOned8192 from "@/components/sections/pj/oned8192";
+import "./page.scss";
+
+
+export const metadata = {
+  title: "1/8192 | Fiber | yslim",
+  description: "My playground",
+};
+
+const PageOned8192 = () => {
+  return (
+    <main className="page">
+      <SectionOned8192/>
+    </main>
+  );
+};
+
+export default PageOned8192;
--- components/sections/home/homeContentLists.js
+++ components/sections/home/homeContentLists.js
@@ -3,6 +3,11 @@ export const inProgressList = [
     categoryName: "In progress",
     urlList: [
       {
+        name: "1/8192",
+        url: "/pj/oned8192",
+        thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
+      },
+      {
         name: "procedural-terrain",
         url: "/fiber/procedural-terrain#debug",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
