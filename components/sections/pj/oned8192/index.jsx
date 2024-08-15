8dcaec147c0135048ab4b5f22d448e2f41d94d33
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-08-15 22:25:50 +0900

    feat: add debug buttons

--- components/sections/pj/oned8192/index.jsx
+++ components/sections/pj/oned8192/index.jsx
@@ -2,27 +2,49 @@
 import { useEffect, useState } from "react";
 import "./SectionOned8192.scss";
 import { Player } from "@lottiefiles/react-lottie-player";
-
+import OeSelector from "./components/ui/OeSelector";
 const SectionOned8192 = () => {
-  const [blueOrGreen, setBlueOrGreen] = useState([""])
+  const [blueOrGreen, setBlueOrGreen] = useState([])
+  const onClickButton = (e, selectorId) => {
+    console.log("Button Clicked", e, selectorId)
+  }
+
+
   useEffect(() => {
     let BGArray = [];
-    for(let i = 0; i < 13; i ++){
+    for (let i = 0; i < 13; i++) {
       const randomAlg = Math.random()
-      if(randomAlg > 0.5) {
+      if (randomAlg > 0.5) {
         BGArray.push("blue")
-      } else{
+      } else {
         BGArray.push("green")
       }
     }
     setBlueOrGreen(BGArray)
-    
-  },[])
 
+  }, [])
+
+  const debugList = (BGArray) => {
+    return BGArray.map((color, index) => {
+      return (
+        <li className="item" key={index}>
+          <div className="item-info">
+            <div>Round: {index}</div>
+            <div>Answer: {color}</div>
+          </div>
+          <div><OeSelector round={index} onClickButton={onClickButton} /></div>
+        </li>
+      )
+    })
+  }
 
   return (
     <div className={"section"}>
-      {blueOrGreen === 'none'? "" : blueOrGreen}
+      {blueOrGreen.length === 0 ?
+        "" :
+        <ul className="oe-debug-list">
+          {debugList(blueOrGreen)}
+        </ul>}
     </div>
   );
 };
