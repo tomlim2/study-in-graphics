47bc2c0da1b62315408fa4f9b19ee16582ec7868
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-09-27 00:50:15 +0900

    feat: get ripple base on mousemove

--- components/sections/pj/ripple-on-click/Experience.jsx
+++ components/sections/pj/ripple-on-click/Experience.jsx
@@ -1,7 +1,13 @@
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
 const Experience = () => {
     // Shaders
+    const raycast = new THREE.Raycaster();
+    let mouse = new THREE.Vector2();
+    const { camera, render } = useThree();
+    const meshRef = useRef();
     const vertexShader = `
 varying vec2 vUv;
 void main() {
@@ -16,7 +22,8 @@ uniform vec2 uMouse;
 uniform float uTime;
 
 void main() {
-  float dist = distance(vUv, uMouse);
+    vec2 mouse = (uMouse / vec2(2.5) + vec2(2.))/2. - vec2(.5,.5);
+  float dist = distance(vUv, mouse);
   float ripple = sin(dist * 20.0 - uTime * 5.0) * 0.1;
   gl_FragColor = vec4(vec3(0.3 + ripple), 1.0);
 }
@@ -25,13 +32,46 @@ void main() {
         uTime: new THREE.Uniform(0),
         uMouse: new THREE.Uniform(new THREE.Vector2(0.5, 0.5)),
     }
+
+    const castDotOnObject = (event) => {
+
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        raycast.setFromCamera(mouse, camera);
+        const intersects = raycast.intersectObject(meshRef.current);
+        if (intersects.length > 0) {
+            const { point } = intersects[0];
+            console.log(point);
+            
+            uniforms.uMouse.value.copy(point);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('mousemove', castDotOnObject)
+
+        return () => {
+            window.removeEventListener('mousemove', castDotOnObject);
+        }
+    }, [])
+
+
+
+    useFrame((state, delta) => {
+        const { mouse, clock } = state;
+        uniforms.uTime.value = clock.elapsedTime;
+        // const vector = new THREE.Vector2(mouse.x, mouse.y);
+        // uniforms.uMouse.value = vector;
+    })
     return (
         <>
-            <mesh>
+            <mesh ref={meshRef}>
                 <planeGeometry args={[5, 5, 100, 100]} />
                 <shaderMaterial
                     vertexShader={vertexShader}
                     fragmentShader={fragmentShader}
+                    uniforms={uniforms}
                 />
             </mesh>
         </>
