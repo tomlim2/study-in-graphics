7f00c9ba95b4dce0d0e0753c4d7384649d753107
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-10-04 20:20:08 +0900

    feat: wrappup the project

--- components/sections/pj/ripple-on-click/Experience.jsx
+++ components/sections/pj/ripple-on-click/Experience.jsx
@@ -1,96 +1,59 @@
-import { useFrame, useThree } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
-import * as THREE from "three";
+import React, { useRef, useEffect } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { shaderMaterial } from '@react-three/drei';
+import * as THREE from 'three';
+import { extend } from '@react-three/fiber';
 
-const Experience = () => {
-    // Shaders
-    const raycast = new THREE.Raycaster();
-    let mouse = new THREE.Vector2();
-    const { camera, render } = useThree();
-    const meshRef = useRef();
-    const vertexShader = `
-varying vec2 vUv;
-void main() {
-  vUv = uv;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
-`;
+const AddTexturesMaterial = shaderMaterial(
+    { texture1: null, texture2: null },
+    `
+        varying vec2 vUv;
 
-    const fragmentShader = `
-varying vec2 vUv;
-uniform vec2 uMouse;
-uniform vec3 uMouseWorldPosition;
-uniform float uTime;
+        void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+    `,
+    `
+        uniform sampler2D texture1;
+        uniform sampler2D texture2;
+        varying vec2 vUv;
 
-void main() {
-    vec2 mwp = uMouseWorldPosition.xy;
-    vec2 mouse = (mwp / vec2(2.5) + vec2(2.))/2. - vec2(.5,.5);
-  float dist = distance(vUv, mouse);
-  float ripple = sin(dist * 20.0 - uTime * 5.0) * 0.1;
-  gl_FragColor = vec4(vec3(0.3 + ripple), 1.0);
-}
-`;
-    const uniforms = {
-        uMouseWorldPosition: new THREE.Uniform(new THREE.Vector3(0, 0, 0)),
-        uTime: new THREE.Uniform(0),
-        uMouse: new THREE.Uniform(new THREE.Vector2(0.5, 0.5)),
-    }
+        void main() {
+            vec4 color1 = texture2D(texture1, vUv);
+            vec4 color2 = texture2D(texture2, vUv);
+            gl_FragColor = color1 + color2;
+        }
+    `
+);
 
-    // const castDotOnObject = (event) => {
+extend({ AddTexturesMaterial });
 
-    //     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    //     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-    //     raycast.setFromCamera(mouse, camera);
-    //     const intersects = raycast.intersectObject(meshRef.current);
-    //     if (intersects.length > 0) {
-    //         const { point } = intersects[0];
-    //         uniforms.uMouse.value.copy(point);
-    //     }
-    // }
+const Plane = () => {
+    const material = useRef();
 
     // useEffect(() => {
-    //     window.addEventListener('mousemove', castDotOnObject)
-
-    //     return () => {
-    //         window.removeEventListener('mousemove', castDotOnObject);
+    //     if (material.current) {
+    //         material.current.texture1 = texture1;
+    //         material.current.texture2 = texture2;
     //     }
-    // }, [])
+    // }, [texture1, texture2]);
 
-
-
-    useFrame((state, delta) => {
-        const { mouse, clock } = state;
-        uniforms.uTime.value = clock.elapsedTime;
-        const vector = new THREE.Vector3(mouse.x, mouse.y, 0.0);
-        vector.unproject(camera);
-        const dir = vector.sub(camera.position).normalize();
-        const distance = -camera.position.z / dir.z;
-        const pos = camera.position.clone().add(dir.multiplyScalar(distance));
-        console.log(pos);
-        
-        uniforms.uMouseWorldPosition.value.copy(pos);
-    })
     return (
-        <>
-            <mesh ref={meshRef}>
-                <planeGeometry args={[5, 5, 100, 100]} />
-                <shaderMaterial
-                    vertexShader={vertexShader}
-                    fragmentShader={fragmentShader}
-                    uniforms={uniforms}
-                />
-            </mesh>
-            {/* <mesh ref={meshRef} position={[3,3,0]}>
-                <planeGeometry args={[5, 5, 100, 100]} />
-                <shaderMaterial
-                    vertexShader={vertexShader}
-                    fragmentShader={fragmentShader}
-                    uniforms={uniforms}
-                />
-            </mesh> */}
-        </>
+        <mesh>
+            <planeGeometry args={[5, 5]} />
+            <addTexturesMaterial ref={material} />
+        </mesh>
     );
-}
+};
 
-export default Experience;  
+const Experience = () => {
+    // const texture1 = new THREE.TextureLoader().load('/path/to/texture1.jpg');
+    // const texture2 = new THREE.TextureLoader().load('/path/to/texture2.jpg');
+
+    return (
+            <Plane />
+    );
+};
+
+export default Experience;
--- components/sections/pj/ripple-on-click/index.jsx
+++ components/sections/pj/ripple-on-click/index.jsx
@@ -3,48 +3,75 @@ import { Canvas } from "@react-three/fiber";
 import "./SectionRippleOnClick.scss";
 import Experience from "./Experience";
 import { useEffect, useRef, useState } from "react";
-import {  Environment } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 import * as THREE from "three";
 
-const SectionRippleOnClick = () => {
-  const [renderer, setRenderer] = useState(null)
-  const canvasRef = useRef();
+const DisplacementCanvas = () => {
+  const canvasWrapRef = useRef();
+
   useEffect(() => {
-    if (canvasRef.current) {
-      const WebGLRenderer = new THREE.WebGLRenderer({
-      })
-      setRenderer(WebGLRenderer)
+    if (!canvasWrapRef.current) return;
+
+    const displacement = {};
+    displacement.canvas = document.createElement("canvas");
+
+    displacement.canvas.style.position = 'fixed'
+    displacement.canvas.style.width = '512px'
+    displacement.canvas.style.height = '512px'
+    displacement.canvas.style.top = 0
+    displacement.canvas.style.left = 0
+    displacement.canvas.style.zIndex = 10
+
+    // Context
+    displacement.context = displacement.canvas.getContext('2d')
+    displacement.context.fillStyle = 'red'
+    displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
+
+    const existingCanvas = canvasWrapRef.current.querySelector("canvas:not([data-reactroot])");
+    if (existingCanvas && existingCanvas.parentNode === canvasWrapRef.current) {
+      canvasWrapRef.current.removeChild(existingCanvas);
     }
+    canvasWrapRef.current.appendChild(displacement.canvas);
+
+    const currentCanvasWrapRef = canvasWrapRef.current;
 
     return () => {
-      setRenderer(null)
-    }
-  }, [])
+      if (displacement.canvas && displacement.canvas.parentNode === canvasWrapRef.current) {
+        currentCanvasWrapRef.removeChild(displacement.canvas);
+      }
+    };
+  }, []);
+  return (
+    <div ref={canvasWrapRef} className={"displacementCanvas"}></div>
+  )
+}
+
+const SectionRippleOnClick = () => {
+  const canvasRef = useRef();
 
   return (
     <div className={"sectionRippleOnClick"}>
-      <>
-        <Canvas
-          ref={canvasRef}
-          camera={{
-            fov: 30,
-            near: 0.1,
-            position: [0, 0, 30],
-          }}>
-          <Environment
-            background={false}
-            files={[
-              "/assets/environmentMaps/2/px.jpg",
-              "/assets/environmentMaps/2/nx.jpg",
-              "/assets/environmentMaps/2/py.jpg",
-              "/assets/environmentMaps/2/ny.jpg",
-              "/assets/environmentMaps/2/pz.jpg",
-              "/assets/environmentMaps/2/nz.jpg",
-            ]}
-          ></Environment>
-          <Experience renderer={renderer} />
-        </Canvas>
-      </>
+      <DisplacementCanvas />
+      <Canvas
+        ref={canvasRef}
+        camera={{
+          fov: 30,
+          near: 0.1,
+          position: [0, 0, 30],
+        }}>
+        <Environment
+          background={false}
+          files={[
+            "/assets/environmentMaps/2/px.jpg",
+            "/assets/environmentMaps/2/nx.jpg",
+            "/assets/environmentMaps/2/py.jpg",
+            "/assets/environmentMaps/2/ny.jpg",
+            "/assets/environmentMaps/2/pz.jpg",
+            "/assets/environmentMaps/2/nz.jpg",
+          ]}
+        ></Environment>
+        <Experience />
+      </Canvas>
     </div>
   );
 };
