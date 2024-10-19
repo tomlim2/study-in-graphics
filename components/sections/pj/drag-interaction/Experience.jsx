c2c638119a2a9d7218f97f0e3717f032e83029fa
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-10-19 22:55:51 +0900

    feat: add physics

--- components/sections/pj/drag-interaction/Experience.jsx
+++ components/sections/pj/drag-interaction/Experience.jsx
@@ -1,19 +1,25 @@
 import * as THREE from "three"
 import { useEffect, useMemo, useRef, useState } from "react";
 
-import fragmentShader from "raw-loader!glslify-loader!shaders/wave-on-click/fragmentShader.glsl";
-import vertexShader from "raw-loader!glslify-loader!shaders/wave-on-click/vertexShader.glsl";
-import simplexNoise4d from 'raw-loader!glslify-loader!shaders/libs/simplexNoise4d.glsl'
-
-import { useControls } from "leva";
-import { mergeVertices } from 'three/addons/utils/BufferGeometryUtils.js'
-import { Bvh, CameraControls, DragControls, OrbitControls } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useDrag } from "@use-gesture/react";
+import { Physics, useBox } from "@react-three/cannon";
 
+function Box(props) {
+    const [ref] = useBox(() => ({
+        mass: 1000,
+        angularVelocity: [0, 0, 0],
+        linearVelocity: [0, 0, 0],
+        ...props
+    }));
+    return (
+        <mesh ref={ref} position={props.position} scale={props.scale} rotation={props.rotation}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshBasicMaterial color="red" wireframe={true} />
+        </mesh>
+    )
+}
 
 function RandomPosObjects() {
-
     const [meshesTransforms] = useMemo(() => {
         const meshesTransforms = []
         for (let i = 0; i < 10; i++) {
@@ -40,12 +46,10 @@ function RandomPosObjects() {
         <>
             {meshesTransforms.map((meshTransform, index) => {
                 return (
-                    <mesh key={index} position={meshTransform.position} scale={meshTransform.scale} rotation={meshTransform.rotation}>
-                        <boxGeometry args={[1, 1, 1]} />
-                        <meshBasicMaterial color="red" wireframe={true} />
-                    </mesh>
+                    <Box key={index} {...meshTransform} />
                 )
             })}
+
         </>
 
     )
@@ -105,13 +109,29 @@ function InteractiveMesh() {
         setIsPointerDown(false);
     }
 
+    const [ref, api] = useBox(() => ({
+        mass: 1,
+        position: [0, 0, 0],
+        type: "Static"
+    }));
+
+    useFrame(() => {
+        if (isPonterDown && draggableObjRef.current) {
+            api.position.copy(draggableObjRef.current.position);
+        }
+    });
+
     return (
         <>
             <mesh onPointerMove={onPointerMove} visible={false}>
                 <planeGeometry args={[100, 100]} />
                 <meshBasicMaterial color="black" side={THREE.DoubleSide} />
             </mesh>
-            <mesh ref={draggableObjRef}
+            <mesh 
+                ref={(mesh) => {
+                    draggableObjRef.current = mesh;
+                    ref.current = mesh;
+                }}
                 onPointerDown={onPointerDown}
                 onPointerUp={onPointerUp}
             >
@@ -122,13 +142,14 @@ function InteractiveMesh() {
     )
 }
 
-
 // Main Scene
 const Experience = () => {
     return (
         <>
-            <RandomPosObjects />
-            <InteractiveMesh />
+            <Physics gravity={[0, 0, 0]}>
+                <RandomPosObjects />
+                <InteractiveMesh />
+            </Physics>
         </>
     );
 }
