ac276b605fdc04cd621fb27d434472d6cebcc4d2
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-10-28 23:13:06 +0900

    refactor: update bounds

--- components/sections/pj/drag-interaction/Experience.jsx
+++ components/sections/pj/drag-interaction/Experience.jsx
@@ -3,47 +3,56 @@ import RandomMeshes from "./RandomMeshes";
 import DraggableMesh from "./DraggableMesh";
 import { OrbitControls } from "@react-three/drei";
 
+function Bounds() {
+    const useWireframe = false;
+    return (
+        <>
+            <RigidBody type="fixed">
+                <mesh position={[0, 0, 8]}>
+                    <boxGeometry args={[100, 100, 4]} />
+                    <meshBasicMaterial wireframe={useWireframe} color="lightblue" visible={false} />
+                </mesh>
+            </RigidBody>
+            <RigidBody type="fixed">
+                <mesh position={[0, 0, -8]}>
+                    <boxGeometry args={[100, 100, 4]} />
+                    <meshBasicMaterial wireframe={useWireframe} color="lightblue" />
+                </mesh>
+            </RigidBody>
+            <RigidBody type="fixed">
+                <mesh position={[0, 30, 0]}>
+                    <boxGeometry args={[100, 4, 80]} />
+                    <meshBasicMaterial wireframe={useWireframe} color="blue" />
+                </mesh>
+            </RigidBody>
+            <RigidBody type="fixed">
+                <mesh position={[-30, 0, 0]}>
+                    <boxGeometry args={[4, 100, 80]} />
+                    <meshBasicMaterial wireframe={useWireframe} color="blue" />
+                </mesh>
+            </RigidBody>
+            <RigidBody type="fixed">
+                <mesh position={[30, 0, 0]}>
+                    <boxGeometry args={[4, 100, 80]} />
+                    <meshBasicMaterial wireframe={useWireframe} color="blue" />
+                </mesh>
+            </RigidBody>
+            <RigidBody type="fixed">
+                <mesh position={[0, -30, 0]}>
+                    <boxGeometry args={[100, 4, 80]} />
+                    <meshBasicMaterial wireframe={useWireframe} color="blue" />
+                </mesh>
+            </RigidBody>
+        </>
+    )
+}
+
 const Experience = () => {
     return (
         <>
             {/* <OrbitControls /> */}
             <Physics gravity={[0, 0, 0]}>
-                <RigidBody type="fixed">
-                    <mesh position={[0, 0, 8]}>
-                        <boxGeometry args={[100, 100, 4]} />
-                        <meshBasicMaterial wireframe={true} color="lightblue" visible={false} />
-                    </mesh>
-                </RigidBody>
-                <RigidBody type="fixed">
-                    <mesh position={[0, 0, -8]}>
-                        <boxGeometry args={[100, 100, 4]} />
-                        <meshBasicMaterial wireframe={true} color="lightblue" />
-                    </mesh>
-                </RigidBody>
-                <RigidBody type="fixed">
-                    <mesh position={[0, 30, 0]}>
-                        <boxGeometry args={[100, 4, 80]} />
-                        <meshBasicMaterial wireframe={true} color="blue" />
-                    </mesh>
-                </RigidBody>
-                <RigidBody type="fixed">
-                    <mesh position={[-30, 0, 0]}>
-                        <boxGeometry args={[4, 100, 80]} />
-                        <meshBasicMaterial wireframe={true} color="blue" />
-                    </mesh>
-                </RigidBody>
-                <RigidBody type="fixed">
-                    <mesh position={[30, 0, 0]}>
-                        <boxGeometry args={[4, 100, 80]} />
-                        <meshBasicMaterial wireframe={true} color="blue" />
-                    </mesh>
-                </RigidBody>
-                <RigidBody type="fixed">
-                    <mesh position={[0, -30, 0]}>
-                        <boxGeometry args={[100, 4, 80]} />
-                        <meshBasicMaterial wireframe={true} color="blue" />
-                    </mesh>
-                </RigidBody>
+                <Bounds />
                 <RandomMeshes />
                 <DraggableMesh />
             </Physics>
