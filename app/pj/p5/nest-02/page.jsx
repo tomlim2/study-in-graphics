4ec87147cd51bdb85e35f5ddb73a8091ecd0df46
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-11-02 21:56:13 +0900

    feat: add p5 flower drawing

--- app/pj/p5/nest-02/page.jsx
+++ app/pj/p5/nest-02/page.jsx
@@ -2,18 +2,18 @@ import "./page.scss";
 
 
 export const metadata = {
-  title: "Face | Nest 01 | P5 | yslim",
+  title: "Flower Drawing | Nest 02 | Fiber | yslim",
   description: "My playground",
 };
 
-const PageP5Nest01 = () => {
+const PageP5Nest02 = () => {
   return (
     <main className="page">
       <div className="p5-iframe-wrapper">
-        <iframe src="https://editor.p5js.org/tomlim2/full/DHu6le7V5"></iframe>
+      <iframe src="https://editor.p5js.org/tomlim2/full/ThCZakfmn"></iframe>
       </div>
     </main>
   );
 };
 
-export default PageP5Nest01;
+export default PageP5Nest02;
