92c652e830daf0c6159241a10f6163870fcbdd41
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-11-03 22:32:15 +0900

    refactor: update homesection codes

--- components/sections/home/homeContentLists.js
+++ components/sections/home/homeContentLists.js
@@ -7,14 +7,14 @@ export const inProgressList = [
         url: "/fiber/physics-in-box",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "drag interaction",
         url: "/pj/drag-interaction",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "p5 flower drawing",
@@ -22,98 +22,98 @@ export const inProgressList = [
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "done",
         tags: [""],
-        description: ""
+        description: "",
       },
       {
         name: "p5 face",
         url: "/pj/p5/nest-01",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "done",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "get vrm metadata",
         url: "/front/get-vrm-metadata",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "render target",
         url: "/pj/render-target",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "water color",
         url: "/pj/water-color",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "ripple on click",
         url: "/pj/ripple-on-click",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "drag and drop",
         url: "/pj/drag-and-drop",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "wave on click",
         url: "/pj/wave-on-click",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "smokes",
         url: "/pj/smokes",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "1/8192",
         url: "/pj/oned8192",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "procedural-terrain",
         url: "/fiber/procedural-terrain#debug",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "glsl 9",
         url: "/shader/glsl-9",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Dynamic wall in Blender",
         url: "/fiber/journey-game3",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Boid in Blender",
         url: "/fiber/journey-game3",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
     ],
   },
@@ -125,14 +125,14 @@ export const inProgressList = [
         url: "/",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "URL simple db system",
         url: "/",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Fluid Simulation",
@@ -140,34 +140,31 @@ export const inProgressList = [
         url2: "https://medium.com/@martinRenou/real-time-rendering-of-water-caustics-59cda1d74aa",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "In water environment",
         url: "/",
         thumbnail: "https://www.shadertoy.com/view/MldfDn",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Toon Render",
         url: "/",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Daily GLSL",
         url: "/",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
     ],
   },
-];
-
-export const historyList = [
   {
     categoryName: "Default",
     urlList: [
@@ -176,42 +173,42 @@ export const historyList = [
         url: "/fiber/sliced-model#debug",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "wobbly-sphere",
         url: "/fiber/wobbly-sphere",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "gpgpu-flow-field",
         url: "/fiber/gpgpu-flow-field",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Default-Glsl",
         url: "/fiber/default-glsl",
         thumbnail: "/assets/images/thumbnails/default-glsl.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Default-Scene",
         url: "/fiber/default",
         thumbnail: "/assets/images/thumbnails/default-scene.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Default-Loader-Gltf",
         url: "/fiber/default-loader-gltf",
         thumbnail: "/assets/images/thumbnails/default-loader-gltf.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
     ],
   },
@@ -223,133 +220,133 @@ export const historyList = [
         url: "/fiber/particle-morphing",
         thumbnail: "/assets/images/thumbnails/particle-morphing.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Particle Cursor",
         url: "/fiber/particle-cursor",
         thumbnail: "/assets/images/thumbnails/particle-cursor.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Earth",
         url: "/fiber/earth",
         thumbnail: "/assets/images/thumbnails/earth.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Model Strawberry Cake",
         url: "/fiber/model-strawberry-cake",
         thumbnail: "/assets/images/thumbnails/model-strawberry-cake.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Model Hugu",
         url: "/fiber/model-fish-hugu",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Halftone Shading",
         url: "/fiber/halftone-shading",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Lights Shading",
         url: "/fiber/lights-shading",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Fireworks",
         url: "/fiber/fireworks",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Hologram",
         url: "/fiber/hologram",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Coffee Smoke",
         url: "/fiber/coffee-smoke",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Text Coffee",
         url: "/fiber/text-coffee",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Test glsl8",
         url: "/shader/glsl-8",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Test glsl7",
         url: "/shader/glsl-7",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Test glsl6",
         url: "/shader/glsl-6",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Test glsl5",
         url: "/shader/glsl-5",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Test glsl4",
         url: "/shader/glsl-4",
         thumbnail: "/assets/images/thumbnails/test-glsl-4.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Test glsl",
         url: "/shader/glsl-1",
         thumbnail: "/assets/images/thumbnails/test-glsl-1.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Test glsl3",
         url: "/shader/glsl-3",
         thumbnail: "/assets/images/thumbnails/glsl3.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Test glsl2",
         url: "/shader/glsl-2",
         thumbnail: "/assets/images/thumbnails/thumbnail_glsl2.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
 
       {
@@ -357,21 +354,21 @@ export const historyList = [
         url: "/shader/vertex-fragnent",
         thumbnail: "/assets/images/thumbnails/vertext-fragnent.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Fragnent-Time",
         url: "/shader/fragnent-time",
         thumbnail: "/assets/images/thumbnails/fragnent-time.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Fragnent-Still",
         url: "/shader/fragnent-still",
         thumbnail: "/assets/images/thumbnails/fragment-still.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
     ],
   },
@@ -383,119 +380,119 @@ export const historyList = [
         url: "/fiber/testToonMaterial",
         thumbnail: "/assets/images/thumbnails/testToonMaterialGradientMap.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Material Sample list",
         url: "/fiber/materials",
         thumbnail: "/assets/images/thumbnails/material-list.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Journey Game 3",
         url: "/fiber/journey-game3",
         thumbnail: "/assets/images/thumbnails/journey-game.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Animating with triangles 4",
         url: "/fiber/animating-tri4",
         thumbnail: "/assets/images/thumbnails/animating-tri4.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Animating with triangles 7",
         url: "/fiber/animating-tri7",
         thumbnail: "/assets/images/thumbnails/animating-tri7.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Drei-Portal",
         url: "/fiber/drei-portal",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Journey-Physics-Base",
         url: "/fiber/journey-physics",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Journey-Physics-GLTF",
         url: "/fiber/journey-physics-gltf",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Journey-Env",
         url: "/fiber/journey-env",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Journey-Env-Model",
         url: "/fiber/journey-env-model",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Journey-Models",
         url: "/fiber/journey-models",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Journey-Stage",
         url: "/fiber/journey-stage",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Journey-Postprocess",
         url: "/fiber/journey-postprocess",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Model emoji 1",
         url: "/fiber/model-emoji-1",
         thumbnail: "/assets/images/thumbnails/model-emoji-1.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Eye with maths",
         url: "/fiber/eye-with-maths",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Gameboy Material 1",
         url: "/fiber/gameboy-material1",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Models",
         url: "/fiber/models",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
 
       {
@@ -503,49 +500,49 @@ export const historyList = [
         url: "/fiber/raymarch101-frag1",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Raymarch101-frag2",
         url: "/fiber/raymarch101-frag2",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Raymarch101-frag3",
         url: "/fiber/raymarch101-frag3",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Raymarch101-frag4",
         url: "/fiber/raymarch101-frag4",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Raymarch101-frag5",
         url: "/fiber/raymarch101-frag5",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Raymarch101-frag6",
         url: "/fiber/raymarch101-frag6",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Raymarch101-frag7",
         url: "/fiber/raymarch101-frag7",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
     ],
   },
@@ -557,7 +554,7 @@ export const historyList = [
         url: "/front//lottie-file/basic",
         thumbnail: "/assets/images/thumbnails/lottie-file-basic-1.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
     ],
   },
@@ -569,14 +566,14 @@ export const historyList = [
         url: "/p5/boid",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "Vector Field",
         url: "/p5/vfa",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
     ],
   },
@@ -588,14 +585,14 @@ export const historyList = [
         url: "/blender/geometry-node",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
       {
         name: "AI Render",
         url: "/blender/ai-render",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
     ],
   },
@@ -607,7 +604,7 @@ export const historyList = [
         url: "/design/ui",
         thumbnail: "/assets/images/thumbnails/img_thumbnail_default.png",
         status: "in progress",
-        tags: [""]
+        tags: [""],
       },
     ],
   },
