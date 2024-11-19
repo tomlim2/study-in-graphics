9679ca32907ae0d6346e775100f8eb5d7768b463
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-11-19 21:24:37 +0900

    feat: update textures

--- components/sections/pj/smokes/Experience.jsx
+++ components/sections/pj/smokes/Experience.jsx
@@ -1,7 +1,10 @@
 import { Perf } from "r3f-perf";
 import LoaderGltf from "./LoaderGltf";
 import Stage from "./Stage";
-import Pattern000 from "./shader/pt0/Pattern000";
+import Pattern000 from "./shader/pt000/Pattern000";
+import Pattern001 from "./shader/pt001/Pattern001";
+import Pattern002 from "./shader/pt002/Pattern002";
+
 
 
 const Experience = () => {
@@ -12,6 +15,8 @@ const Experience = () => {
       {/* <CameraControls makeDefault maxDistance={35} dollySpeed={0.25} /> */}
       <Stage />
       <Pattern000 />
+      <Pattern001 />
+      <Pattern002 />
       <LoaderGltf />
     </>
   );
---
+++ components/sections/pj/smokes/shader/pt001/Pattern001.jsx
@@ -0,0 +1,40 @@
+import { extend, useFrame, useLoader } from '@react-three/fiber'
+import fragmentShader from 'raw-loader!glslify-loader!./fragment.glsl'
+import vertexShader from 'raw-loader!glslify-loader!./vertex.glsl'
+import { DoubleSide, RepeatWrapping, TextureLoader } from "three";
+import { useRef } from "react";
+
+export default function Pattern001() {
+    const materialRef = useRef()
+    const perlinTexture = useLoader(TextureLoader, "/coffeeSmoke/perlin.png")
+    perlinTexture.wrapS = RepeatWrapping
+    perlinTexture.wrapT = RepeatWrapping
+    console.log(perlinTexture);
+    useFrame((state, delta) => {
+        if (materialRef.current) {
+            materialRef.current.uniforms.uTime.value = state.clock.elapsedTime;
+            // console.log(materialRef.current.uniforms.uTime.value, "hi");
+        }
+    });
+    return (
+        <>
+            <mesh position={[-2, 0, 0]} scale={[1, 1, 1]}>
+                <planeGeometry args={[1, 1, 16, 32]} />
+                <shaderMaterial
+                    ref={materialRef}
+                    vertexShader={vertexShader}
+                    fragmentShader={fragmentShader}
+                    transparent={true}
+                    uniforms=
+                    {
+                        {
+                            uTime: { value: 0 },
+                            uPerlinTexture: { value: perlinTexture }
+                        }}
+                    // wireframe={true} 
+                    depthWrite={false}
+                    side={DoubleSide} />
+            </mesh>
+        </>
+    )
+}
