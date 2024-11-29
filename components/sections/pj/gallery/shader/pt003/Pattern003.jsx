0d0148b764261dbe08ff85354e461b719beeb3c8
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2024-11-29 22:15:23 +0900

    feat: add debug sun

--- components/sections/pj/gallery/shader/pt003/Pattern003.jsx
+++ components/sections/pj/gallery/shader/pt003/Pattern003.jsx
@@ -1,34 +1,54 @@
 import { extend, useFrame, useLoader } from '@react-three/fiber'
 import fragmentShader from 'raw-loader!glslify-loader!./fragment.glsl'
 import vertexShader from 'raw-loader!glslify-loader!./vertex.glsl'
-import { DoubleSide, RepeatWrapping, SRGBColorSpace, TextureLoader } from "three";
 import { useRef } from "react";
 import * as THREE from 'three';
 
 export default function Pattern003() {
     const materialRef = useRef()
-    const perlinTexture = useLoader(TextureLoader, "/coffeeSmoke/perlin.png")
-    perlinTexture.wrapS = RepeatWrapping
-    perlinTexture.wrapT = RepeatWrapping
+    const debugSunRef = useRef()
+    const perlinTexture = useLoader(THREE.TextureLoader, "/coffeeSmoke/perlin.png")
+    perlinTexture.wrapS = THREE.RepeatWrapping
+    perlinTexture.wrapT = THREE.RepeatWrapping
+
+    // Coordinates
+    const sunSpherical = new THREE.Spherical(1, Math.PI * 0.5, 0.5)
+    const sunDirection = new THREE.Vector3()
+
+    const updateSun = () => {
+        // Sun direction
+        sunDirection.setFromSpherical(sunSpherical)
+
+        // Debug
+        if (debugSunRef.current) { debugSunRef.current.position.copy(sunDirection).multiplyScalar(5) }
+    }
+
+
+
     useFrame((state, delta) => {
+        updateSun()
         if (materialRef.current) {
             materialRef.current.uniforms.uTime.value = state.clock.elapsedTime;
             // console.log(materialRef.current.uniforms.uTime.value, "hi");
         }
     });
 
-    const earthDayTexture = useLoader(TextureLoader, "/earth/day.jpg")
-    earthDayTexture.colorSpace = SRGBColorSpace
+    const earthDayTexture = useLoader(THREE.TextureLoader, "/earth/day.jpg")
+    earthDayTexture.colorSpace = THREE.SRGBColorSpace
     earthDayTexture.anisotropy = 8
-    const earthNightTexture = useLoader(TextureLoader, "/earth/night.jpg")
-    earthNightTexture.colorSpace = SRGBColorSpace
+    const earthNightTexture = useLoader(THREE.TextureLoader, "/earth/night.jpg")
+    earthNightTexture.colorSpace = THREE.SRGBColorSpace
     earthNightTexture.anisotropy = 8
-    const earthCloudTexture = useLoader(TextureLoader, "/earth/specularClouds.jpg")
-    earthCloudTexture.colorSpace = SRGBColorSpace
+    const earthCloudTexture = useLoader(THREE.TextureLoader, "/earth/specularClouds.jpg")
+    earthCloudTexture.colorSpace = THREE.SRGBColorSpace
     earthCloudTexture.anisotropy = 8
 
     return (
         <>
+            <mesh ref={debugSunRef}>
+                <icosahedronGeometry args={[0.1, 2]} />
+                <meshBasicMaterial />
+            </mesh>
             <mesh position={[-1, 1, 0]} scale={[1, 1, 1]}>
                 <sphereGeometry args={[.5, 16, 32]} />
                 <shaderMaterial
@@ -47,7 +67,7 @@ export default function Pattern003() {
                         }}
                     // wireframe={true} 
                     depthWrite={false}
-                    side={DoubleSide} />
+                    side={THREE.DoubleSide} />
             </mesh>
         </>
     )
